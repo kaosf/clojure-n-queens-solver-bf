9d2871f9713a5bfb5b517d53c1ed7cef9e1a9127
ka <ka.kaosf@gmail.com>
2013-11-11 00:14:54 +0900

    fix the URL of MIT license in project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "MIT"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.combinatorics "0.0.6"]]
   :main ^:skip-aot n-queens-bf.core
