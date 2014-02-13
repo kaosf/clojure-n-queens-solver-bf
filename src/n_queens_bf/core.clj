a2c05a985a7b52e506529e2599aed5668f1bca9b
ka <ka.kaosf@gmail.com>
2014-02-13 21:27:45 +0900

    Revert "use ":refer" instead of ":as" and remove "comb/""
    This reverts commit 0811f17d36f16d8f2a249058ce8a14fd091467b2.

--- src/n_queens_bf/core.clj
+++ src/n_queens_bf/core.clj
@@ -1,15 +1,15 @@
 (ns n-queens-bf.core
-  (:require [clojure.math.combinatorics :refer (combinations permutations)])
+  (:require [clojure.math.combinatorics :as comb])
   (:gen-class))
 
 (defn columns-of [board size] (for [i (range size) :let [vi (nth board i)]] [(inc i) vi]))
 
 (defn naname? [[[i vi] [j vj]]] (= (Math/abs (- i j)) (Math/abs (- vi vj))))
 
-(defn ok? [board size] (not-any? naname? (combinations (columns-of board size) 2)))
+(defn ok? [board size] (not-any? naname? (comb/combinations (columns-of board size) 2)))
 
 (defn make-solutions [n]
-  (let [candidates (permutations (range 1 (inc n)))]
+  (let [candidates (comb/permutations (range 1 (inc n)))]
     (filter (fn [board] (ok? board n)) candidates)))
 
 (defn -main
